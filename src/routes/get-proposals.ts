4e6aba960d0dccf21446252f2b387d5695d92144
dennis <dns@dexie.space>
2024-07-02 15:12:10 +0200

    encode keys, select explicit columns

--- src/routes/get-proposals.ts
+++ src/routes/get-proposals.ts
@@ -2,8 +2,9 @@ import { Context } from 'hono';
 
 export async function getProposals(c: Context) {
   const proposals = await c.env.DB.prepare(
-    `select *, hash as id,
-     (select json_group_array(json_object('id', id, 'choice', choice, 'amount',
+    `select hash as id, title, content, discussion_link, start_height, end_height, date_created,
+    lower(hex(deployer_pubkey)) AS deployer_pubkey, lower(hex(deployer_signature)) AS deployer_sig,
+    (select json_group_array(json_object('id', id, 'choice', choice, 'amount',
       ifnull((select sum(amount) / 1000.0 from gov_votes where choice = gov_choices.id and proposal_id = gov_proposals.id), 0)))
       from gov_choices WHERE proposal_id = gov_proposals.id order by id) as results
     from gov_proposals order by id`
